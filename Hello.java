0e15f0b6b28590d9bfd03d4c581424531ad4010e
AMIL  M SHAJI <95700607+amilmshaji@users.noreply.github.com>
2022-10-19 21:22:22 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
-        String _‎ = "Hello ";
-        String _‏ = "World";
+        String _‎ = "Hello  ";
+        String _‏ = "World how are you";
         String _‎‏ = " !!";
         System.out.println(_‎+_‏+_‎‏);
     }
